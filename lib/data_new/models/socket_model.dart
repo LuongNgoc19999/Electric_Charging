45af5390b0b235e0b153d371a7166a3b83ba026b
ngoc <ngoc27499@gmail.com>
2025-11-24 21:46:30 +0700

    add map

---
+++ lib/data_new/models/socket_model.dart
@@ -0,0 +1,14 @@
+
+class SocketModel {
+  final String id;
+  final double powerKw;
+  final bool isAvailable;
+  final double price;
+
+  SocketModel({
+    required this.id,
+    required this.powerKw,
+    required this.isAvailable,
+    required this.price,
+  });
+}
---
+++ lib/data_new/models/user_model.dart
@@ -0,0 +1,19 @@
+class UserModel {
+  UserModel({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.password,
+    required this.phoneNumber,
+    required this.address,
+  });
+
+  String id;
+  String firstName;
+  String lastName;
+  String email;
+  String password;
+  String phoneNumber;
+  String address;
+}
